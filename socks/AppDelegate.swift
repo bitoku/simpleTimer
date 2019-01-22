05939a9d5336e893c593015d6be4be5b953f375d
Ayato Tokubi <tokubi.ayato@gmail.com>
2019-01-22 23:05:19 +0900

    Initial Commit

---
+++ socks/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  socks
+//
+//  Created by 徳備彩人 on 2019/01/22.
+//  Copyright © 2019 徳備彩人. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ socks/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  socks
+//
+//  Created by 徳備彩人 on 2019/01/22.
+//  Copyright © 2019 徳備彩人. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
